276f86735011bb8185e0c3cac0e5c2e358672ec1
Landrian61 <luswataandrew190@gmail.com>
2024-05-29 12:40:53 +0300

    Signup component

--- src/app/ui/signup/signupflow.tsx
+++ src/app/ui/signup/signupflow.tsx
@@ -1,11 +1,39 @@
 import React from 'react'
 
-const signupflow = () => {
+function Flow() {
   return (
-    <div>
-      
+    <div className="w-1/2 flex flex-col justify-center items-center bg-gray-100 p-8">
+    <div className="text-3xl font-bold mb-8">NovaCRM</div>
+    <ul className="space-y-4">
+      <li className="flex items-center">
+        <span className="material-icons">account_circle</span>
+        <span className="ml-2">Your Details</span>
+      </li>
+      <li className="flex items-center">
+        <span className="material-icons">email</span>
+        <span className="ml-2">Verify email</span>
+      </li>
+      <li className="flex items-center">
+        <span className="material-icons">group</span>
+        <span className="ml-2">Your workspace</span>
+      </li>
+      <li className="flex items-center">
+        <span className="material-icons">flight_takeoff</span>
+        <span className="ml-2">Welcome to NovaCRM</span>
+      </li>
+    </ul>
+    <div className="flex mt-8 space-x-4">
+      <a href="/">
+        <a className="underline">Back to Home</a>
+      </a>
+      <a href="/signin">
+        <a className="underline">Signin</a>
+      </a>
     </div>
+  </div>
   )
 }
 
-export default signupflow
+export default Flow
+
+
